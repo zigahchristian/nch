528270fe982d6cea223a078e80c7a3d0fcc40019
Zigah Christian <zigahchristian@yahoo.com>
2025-09-29 13:13:57 +0000

    New Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "com.example.nch"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+ 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
