66fb0110c73c9dd836aaff05cb24e2d797ba84e0
Zigah Christian <zigahchristian@yahoo.com>
2025-10-02 19:16:50 +0000

    Clean up contact page

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -14,37 +14,37 @@ class ContactScreen extends StatelessWidget {
             const SizedBox(height: 20),
             const Text(
               'Contact Us',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
             _buildContactCard(
               icon: Icons.email,
-              title: 'Email',
-              subtitle: 'Send us an email',
+              title: '',
+              subtitle: '',
               value: 'support@newcatholichymnal.com',
             ),
             const SizedBox(height: 16),
             _buildContactCard(
               icon: Icons.phone,
-              title: 'Phone',
-              subtitle: 'Call us',
+              title: '',
+              subtitle: '',
               value: '024 000 0000 / 020 000 0000',
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 10),
             _buildContactCard(
               icon: Icons.location_on,
-              title: 'Address',
-              subtitle: 'Visit us',
-              value: 'Queen Of Peace Catholic Church - Madina, Accra - Ghana',
+              title: '',
+              subtitle: '',
+              value: 'Queen of Peace Catholic Church - Madin, Accra - Ghana',
             ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 10),
             const Text(
-              'About This App',
+              'About The New Catholic Hymnal 2021',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
             const Text(
-              'The NewCatholic Hymnal is a digital collection of traditional updated Catholic hymns '
+              'The New Catholic Hymnal is a digital collection of traditional updated Catholic hymns '
               'designed to help you sing, worship and pray through Catholic Mass.',
               style: TextStyle(fontSize: 16),
             ),
@@ -66,7 +66,10 @@ class ContactScreen extends StatelessWidget {
         leading: Icon(icon, color: Colors.teal),
         title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(subtitle),
-        trailing: Text(value, style: const TextStyle(color: Colors.teal)),
+        trailing: Text(
+          value,
+          style: const TextStyle(color: Color.fromARGB(255, 125, 131, 130)),
+        ),
         onTap: () {
           // Handle contact action
         },
